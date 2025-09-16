d45b34571bf407e21437e436225016ea70f6f326
giblim <giblimdev@gmail.com>
2025-09-16 19:05:50 +0200

    instal

---
+++ app/user/documents/[id]/rituels/Emulation/instalation/annex/page.tsx
@@ -0,0 +1,165 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h2>Annexe 1 :</h2>
+      <p>
+        I30.jpg Transcription ANNEXE 1 : EXHORTATIONS Dans tous les cas
+        (Installation, Réinstallation ou Reconduction), elles devront être
+        présentées par cœur. Ces Exhortations seront données en présence de tous
+        les Frères, une fois terminée l'installation du Collège des Officiers de
+        la Loge. EXHORTATION AU VÉNÉRABLE MAÎTRE Depuis l'Occident, à
+        l'emplacement propre au Rite. Le V.M. reste assis. V.F. Vénérable
+        Maître, puisque vous venez d'être installé dans la Chaire de cette digne
+        et respectable Loge, vous ne pouvez être insensible ni aux obligations
+        qui vous incombent comme son chef, ni aux responsabilités que vous
+        assumerez en vous acquittant fidèlement des devoirs de votre charge.
+        L'Honneur, la réputation et l'utilité de cette Loge dépendront en grande
+        partie de l'adresse et de l'assiduité avec lesquelles vous dirigerez ses
+        affaires, tandis que le bonheur de ses membres augmentera en proportion
+        du zèle et de l'habileté avec lesquels vous propagerez les véritables
+        principes de notre Institution. Prenez pour modèle l'astre glorieux de
+        la Nature qui, se levant à l'Orient, répand d'une façon continue de la
+        lumière et de l'éclat sur tout ce qui l'entoure. De même, vous devez
+        communiquer la lumière et la connaissance aux Frères de votre Loge.
+        I31.jpg Transcription Imprégnez leur esprit de la grandeur et de la
+        haute portée de la Maçonnerie, enjoignez-leur de ne jamais la
+        déshonorer, ordonnez-leur de pratiquer, au-dehors, les devoirs qui leur
+        ont été inculqués au sein de la Loge, et de prouver au monde, par une
+        conduite vertueuse, affable et discrète, l'heureuse et bienfaisante
+        influence de notre ancienne Institution ; afin qu'au seul nom de Maçon,
+        le monde soit convaincu que celui qui le porte est un de ceux à qui les
+        affligés peuvent confier leur peine, à qui les malheureux peuvent
+        demander aide et protection, un de ceux dont le bras est guidé par la
+        justice et dont le cœur déborde d'Amour. Enfin, Vénérable Maître, en
+        suivant strictement le Règlement Intérieur de votre Loge, les
+        Constitutions de la Maçonnerie, et, par-dessus tout, en vous conformant
+        aux enseignements des Saintes Écritures, données comme règle et comme
+        guide de notre Foi, vous tresserez une couronne de gloire et de bonheur
+        qui ornera encore votre souvenir, lorsque le temps n'aura plus de limite
+        pour vous. Se mettre au Signe de Foi et s'incliner légèrement. Puisse
+        Dieu vous accorder la santé et la force pour remplir les devoirs de
+        votre haute fonction, à votre satisfaction et au plus grand profit de
+        votre Loge.
+      </p>
+      <p>
+        EXHORTATION AUX SURVEILLANTS Depuis la Colonne du Nord, à l'emplacement
+        propre au Rite. Les Surveillants se lèvent. V.F. Frères 1ᵉʳ et 2ᵈ
+        Surveillants, vous êtes suffisamment imprégnés des principes de la
+        Maçonnerie pour écarter toute crainte que vous ne soyez pas à la hauteur
+        des devoirs de votre charge. Je me bornerai donc à vous recommander
+        d'imiter, avec soin, ce que vous rencontrerez de louable chez les autres
+        et de corriger, en vous, ce qui vous paraîtra défectueux chez autrui.
+        Vous devez être des modèles de bon ordre et de ponctualité, car c'est
+        seulement en conformant notre propre conduite aux lois, que nous pouvons
+        raisonnablement exiger des autres qu'ils leur obéissent. Vous devez
+        assister, avec zèle, le Vénérable Maître dans l'accomplissement des
+        devoirs de son importante fonction, en éclairant de vos lumières et de
+        votre savoir tous ceux qu'il placera sous votre direction. À considérer
+        les bonnes dispositions dont vous avez fait preuve jusqu'ici, nous
+        sommes persuadés que votre conduite vous méritera certainement l'estime
+        de vos Frères et le glorieux témoignage d'une conscience pure. Veuillez
+        vous asseoir. Les Surveillants s'assoient.
+      </p>
+      <p>
+        EXHORTATION AUX FRÈRES Depuis l'Orient, à l'emplacement propre au Rite.
+        V.F. Vénérable Maître, je vous prie de demander aux Frères de se lever.
+        V.M. ▼ Tous les Frères de la Loge se lèvent (hormis le V.M. et les
+        Surveillants). V.F. Mes Frères, la nature de notre Constitution est
+        ainsi conçue que, nécessairement, d'aucuns sont appelés à commander et à
+        enseigner, et que d'autres doivent apprendre, se soumettre et obéir.
+        L'humilité, pour chacun, est une qualité essentielle. Les Frères que le
+        Vénérable Maître a choisis pour l'assister dans la direction et le
+        gouvernement de la Loge, connaissent trop bien les principes de la
+        Franc-Maçonnerie et les lois de notre institution pour qu'il y ait lieu
+        de craindre qu'ils ne soient à la hauteur des devoirs de leur charge, ou
+        qu'ils n'excèdent les pouvoirs qui leur ont été confiés. Mes Frères,
+        vous avez le cœur trop généreux pour vous montrer envieux de leur
+        avancement. J'espère donc que nous n'aurons qu'un seul but, celui de
+        nous rendre mutuellement agréables, et de nous unir dans le noble
+        dessein d'être heureux et de communiquer le bonheur. Et puisque cette
+        association a été formée avec tant d'unanimité et de concorde,
+        puisse-t-elle durer longtemps. Puissent l'Affection et l'Amour
+        fraternels nous distinguer, à tout jamais, comme hommes et comme Maçons.
+        I34.jpg Transcription C'est ainsi, mes Frères, que nous apprendrons à
+        être doux et humbles, à être fidèles à notre Dieu, à notre patrie et à
+        nos lois, à verser une larme de compassion sur les faiblesses d'un Frère
+        et à répandre le baume bienfaisant de la consolation dans le sein des
+        affligés. Puissent ces principes et ces règles se transmettre en cette
+        Loge, purs et sans taches, de génération en génération. V.F. Vénérable
+        Maître, veuillez faire asseoir les Frères. V.M. ▼ Tous les Frères
+        s'assoient.
+      </p>
+      <h2>Annexe 2 :</h2>
+      <p>
+        Annexe 2 : Commentaires I - Adoniram Une variante britannique donne
+        l'explication suivante à propos d'Adoniram : « Adoniram fut employé
+        pendant sept ans dans les collines du Liban à superviser le travail de
+        la corvée levée d'Israël. Il avait la charge de quelque 30 000 ouvriers,
+        qui façonnaient la pierre et le bois pour les rendre prêts à l'emploi
+        dans le Temple où on les assemblait avec des outils de bois, de sorte
+        qu'on n'entendait résonner aucun outil de métal sur le chantier. La
+        tradition rapporte que bien que l'ouvrage ait été préparé à une si
+        grande distance, quand les pièces furent assemblées à Jérusalem, chaque
+        partie s'ajustait si exactement à ses voisines que quand l'ensemble fut
+        achevé, il apparut plus comme l'œuvre du G.A.D.L.U. que comme une œuvre
+        humaine. Après la mort prématurée d'Hiram Abif, Adoniram devint le
+        principal architecte du Temple. C'est pour toutes ces raisons que le Roi
+        Salomon voulut le présenter à la Reine de Saba et le récompenser
+        publiquement par le titre de Ghiblim » (Voir I Rois IV, 6 ; I Rois V,
+        27-32, trad. Osty ou V, 13-18, trad. Segond et I Rois VI, 7). II -
+        Variantes dans la présentation des signes de Maître Installé Il existe
+        deux principales variantes du salut. Variante 1 : En s'inclinant et en
+        fléchissant le genou droit, faire partir la main droite de l'épaule
+        gauche, index et majeur tendus, les autres doigts repliés, et abaisser
+        la main droite en arc vers le côté droit du corps. Variante 2 : Même
+        chose, mais tous les doigts sont étendus et l'on part du front. Il peut
+        être utile aux Maçons voyageurs de savoir qu'une des variantes
+        britanniques de l'installation explique les signes de la façon suivante,
+        qui présente quelques différences avec le présent rituel : Il y a cinq
+        signes réservés au Maître Installé, quatre d'entre eux sont de
+        circonstance, et un est pénal. Le premier signe de circonstance est le
+        signe d'Appel et il est donné trois fois. C'est le geste d'un monarque
+        plein de majesté invitant son sujet à s'approcher de lui. On le fait
+        trois fois, car le Roi Salomon appela trois fois Adoniram. Le deuxième
+        signe de circonstance est donné en étendant la main comme si l'on tenait
+        un Fil à plomb (On fait le signe). Ceci fait allusion à un passage de
+        l'Ancien Testament où le Seigneur dit : « Je ferai descendre un fil à
+        plomb au milieu de mon peuple » (Amos VII, 7-8). Pendant votre
+        Obligation, les Frères se tenaient autour de vous dans cette attitude
+        pour indiquer que vous êtes symboliquement devenu un Fil à plomb au
+        milieu de votre Loge. Les Frères attendent de vous que vous leur soyez
+        un exemple tout autant qu'un guide. Le troisième signe est le signe du
+        Silence. Il est donné en plaçant l'index sur les lèvres (On fait le
+        signe). De même que le Fil à plomb sera le symbole de votre conduite, de
+        même ce signe sera celui de la prudence de votre langage. Vous tiendrez
+        votre langue en bride et vous placerez un sceau sur vos lèvres. Le
+        quatrième signe est le Salut d'un Maître ès arts et ès sciences. Il est
+        exécuté en faisant un pas en arrière du pied droit, en s'inclinant et en
+        saluant trois fois de la main à partir du front, ce qui fait allusion à
+        la façon dont Adoniram salua le Roi Salomon. De même, par une étude
+        constante des détails du Plan Divin tel qu'il est tracé dans le Livre de
+        la Loi Sacrée, non seulement vous progresserez vous-même, mais vous
+        serez aussi capable d'aider les Frères à réaliser dans une certaine
+        mesure la volonté du Grand Architecte De L'Univers qui a fait l'homme à
+        Son Image. IV - Le mot Ghiblim ou Giblim Ce nom francisé en Ghiblite,
+        Ghibléen ou Guiblien, désigne les habitants de la région de Guébal. Les
+        ouvriers du Roi Hiram de Tyr, qui taillèrent les pierres pour le Temple
+        du Roi Salomon, furent recrutés parmi les Guibliens (voir I Rois V,
+        17-18, trad. Segond ou V, 31-32, trad. Osty). V - Le mot de passe Le mot
+        rabouni, ou mieux rabbouni, est une forme secondaire de rabbi,
+        constituée du substantif araméen rabbûn avec le suffixe de la première
+        personne du singulier. L'appellation rabbi reconnaît l'autorité d'un
+        Maître qui enseigne, l'appellation rabbouni celle d'un Maître qui
+        commande ; en français, « Maître » désigne un enseignant, mais aussi un
+        patron ou un propriétaire. L'emploi en araméen judéo-palestinien de la
+        forme rabbouni appliquée à un grand personnage, voire à Dieu lui-même,
+        montre qu'il s'agit là d'une forme de respect. Il signifie
+        originellement « mon Maître », mais la valeur du possessif est effacée,
+        à l'instar des termes français « Monsieur » ou « Monseigneur ».
+      </p>
+      <h2>Annexe 3 :</h2>
+    </div>
+  );
+}
---
+++ app/user/documents/[id]/rituels/arche/AIJ/pagre.tsx
@@ -0,0 +1,295 @@
+import React from "react";
+
+export default function pagre() {
+  return (
+    <div>
+      <h1>Cérémonie d'Installation du Troisième principal</h1>
+      <p className="com">
+        {" "}
+        C'est à chaque Principal qu'il appartient normalement d'installer son
+        successeur. Si le D.C. du Chapitre n'est pas Passé Z., le T.E.Z. désigne
+        un P.Z. comme D.C. Installateur (D.C.I.). Ce dernier qui avait été
+        préalablement prévenu de sa désignation aura soin de préparer, avant la
+        cérémonie, de l'huile, un encensoir, une cuvette remplie d'eau et un
+        linge blanc ; il déposera le tout sur la table du Scribe Néhémie. Après
+        avoir prié tous les Compagnons au-dessous du rang de Troisième Principal
+        Installé (sauf le J. élu) de se retirer, le T.E.Z. procède ainsi :
+      </p>
+      <p>
+        T.E.Z. : - O - O - O - O - Compagnons, je déclare cette assemblée
+        régulièrement constituée en un conclave de Troisièmes Principaux
+        Installés.
+      </p>
+      <p className="com">
+        Le D.C.I. conduit le J. élu à l'Extrémité Ouest du Chapitre et se tient
+        à sa droite. Un encensoir, si l'on peut s'en procurer un, ajoute à la
+        solennité de cette imposante cérémonie.
+      </p>
+      <p>
+        <b>DCI :</b>
+        <span className="com">(Au J élu)</span>Déchausse-toi car le sol sur
+        laquel tu te tiens est sacré.
+      </p>
+      <p className="com">
+        (le J élu obéi et le DCI lui présente la cuvette d'eau et le linge.)
+      </p>
+      <p>
+        <b>D.C.I. : </b>Répétez après moi : Je lave mes mains dans l'Innocence,
+        et je vais autour de Ton autel, ô Éternel.
+      </p>
+      <p>
+        <b>J. élu : </b>Je lave mes mains dans l'Innocence, et je vais autour de
+        Ton autel, ô Éternel.
+      </p>
+      <p className="com">
+        (Le D.C.I. (à voix basse) invite le J. élu à l'accompagner pour faire le
+        tour de l'autel. Ils marcheront côte à côte, le D.C.I. ne prend pas la
+        main du J. élu.)
+      </p>
+      <p>
+        <b>D.C.I. : </b>
+        <span className="com">(Au T.E.Z.) </span>T.E., je vous présente le
+        Compagnon..., régulièrement élu J. de ce Chapitre, pour recevoir le
+        bénéfice de l'Installation.
+      </p>
+      <p>
+        <b>P.I. : </b>
+        <span className="com">(se lève)</span> Excellent Compagnon, votre
+        présentation sera examinée avec attention, mais je dois demander à notre
+        Digne Compagnon..., le mot de passe conduisant à la Chaire de Troisième
+        Principal, qui est le mot d'un Maître Installé.
+      </p>
+      <p>
+        <b>J. élu : </b> « Giblin ».
+      </p>
+      <p>
+        <b>P.I. : </b>Digne Compagnon, je vous félicite de l'honneur qui vous a
+        été fait par le Chapitre. Vos Compagnons vous ont récompensé, approuvant
+        en même temps votre conduite d'homme ainsi que votre zèle et votre
+        fidélité de Maçon. Avant de procéder à votre Installation, je dois vous
+        demander si vous consentez dès à présent à accepter les devoirs de la
+        haute charge à laquelle vous avez été élu et à vous en acquitter avec
+        zèle et fidélité ?
+      </p>
+      <p>
+        <b>J. élu : </b>J'y consens.
+      </p>
+      <p>
+        <b>P.I. : </b>Promettez-vous d'agir de concert avec les deux autres
+        Principaux pour maintenir la dignité de cet Ordre ainsi que la paix,
+        l'harmonie et l'unité de ce Chapitre dont vous allez devenir l'un des
+        chefs ?
+      </p>
+      <p>
+        <b>J. élu : </b> Je le promets.
+      </p>
+      <p>
+        <b>P.I. : </b>Promettez-vous d'observer et de faire observer les Lois et
+        les Règlements du Suprême Grand Chapitre ainsi que ceux établis dans ce
+        Chapitre ?
+      </p>
+      <p>
+        <b>J. élu : </b>Je le promets.
+      </p>
+      <p>
+        P.I. : Promettez-vous de rendre la justice avec impartialité, et de
+        réprimer la désobéissance, fermement et raisonnablement, en n'oubliant
+        jamais que la Clémence est l'attribut céleste de Celui à qui notre
+        Temple est dédié ?
+      </p>
+      <p>J. élu : Je le promets. </p>
+      <p>
+        P.I. : Consentez-vous à prendre l'Obligation Solennelle de ne jamais
+        révéler mal à propos aucun des secrets ou mystères qui peuvent vous être
+        confiés maintenant ou plus tard comme Principal de l'Ordre ?{" "}
+      </p>
+      <p>J. élu : J'y consens. </p>
+      <p>
+        P.I. : Veuillez donc vous avancer vers le côté Nord de l'Autel,
+        agenouillez-vous sur les deux genoux, placez vos deux mains sur le
+        Triangle, déclinez vos nom et prénoms et répétez après moi :
+      </p>
+      <h2>OBLIGATION</h2>
+      <p>
+        <span className="com">
+          (Tous se lèvent et effectuent la 2e partie du signe de Respect ou
+          d'Appel.)
+        </span>
+        Moi,..., en présence de Dieu tout plein de miséricorde, et de ce
+        conclave de Principaux Installés, de mon plein gré et consentement, par
+        ceci et sur ceci
+        <span className="com">
+          (Le P.I. pose un instant son sceptre sur les mains du J. élu, puis sur
+          le Triangle)
+        </span>
+        , je promets et jure solennellement, de ne jamais révéler, les secrets
+        particuliers à la Chaire de Troisième Principal, à qui que ce soit, sauf
+        à un Compagnon de ce Suprême Degré, qui aura été dûment élu ou nommé à
+        cette charge, par une autorité légalement constituée, et cela seulement,
+        avec le concours de deux ou plusieurs Principaux Installés,
+        régulièrement assemblés à cet effet. <br />
+        Je promets en outre solennellement, de toujours observer, et de faire
+        observer, pour autant que cela sera en mon pouvoir, les Lois et les
+        Ordonnances du Suprême Grand Chapitre, de me conformer aux rites et
+        cérémonies établis dans l'Ordre, et de ne jamais accepter d'exalter
+        aucun candidat à ce Suprême Degré, si ce n'est avec l'entière
+        conviction, qu'il est digne de cet honneur. <br />
+        Sur l'observation de tous ces points{" "}
+        <span className="com">
+          (Le P.I. touche à nouveau de son sceptre les mains du J. élu puis le
+          Triangle)
+        </span>
+        , je prends un engagement solennel, sous peine des châtiments indiqués
+        dans toutes mes obligations, et je prie le Très Haut, de m'armer de
+        constance et de fermeté, pour tenir cette Obligation Solennelle de
+        Troisième Principal.
+      </p>
+      <p>
+        <b> P.I. : </b>Comme gage de votre fidélité et pour que cet engagement
+        devienne une Obligation Solennelle, scellez-le de vos lèvres une fois
+        sur le Triangle.{" "}
+        <span className="com">(Le P.I. lui fait signe avec son sceptre.)</span>{" "}
+        <br />
+        Levez-vous, Troisième Principal dûment assermenté.
+        <span className="com">
+          Le P.I. lui passe son sceptre sous les poignets pour l'y aider.
+        </span>
+        <br />
+        Veuillez maintenant vous agenouiller devant la Chaire du Troisième
+        Principal et écouter avec attention la lecture de quelques passages des
+        Saintes Écritures.
+      </p>
+      <p className="com">
+        Tous s'assoient. br C'est le P.I. qui doit procéder à la lecture ou,
+        éventuellement, un Passé Z.{" "}
+      </p>
+      <p>
+        <h3>LÉVITIQUE VIII - versets 1 à 12 </h3>
+        <em>
+          L'Éternel parla à Moïse et dit : <br />
+          « Prends Aaron et ses fils avec lui, les vêtements, l'huile d'onction,
+          le taureau expiatoire, les deux béliers et la corbeille de pains sans
+          levain ; et convoque toute l'assemblée à l'entrée de la tente
+          d'assignation. » <br />
+          Moïse fit ce que l'Éternel lui avait ordonné ; et l'assemblée se
+          réunit à l'entrée de la tente d'assignation. Moïse dit à l'assemblée :
+          « Voici ce que l'Éternel a ordonné de faire. » Moïse fit approcher
+          Aaron et ses fils et il les lava avec de l'eau. Page 11 Il mit à Aaron
+          la tunique, il le ceignit de la ceinture, il le revêtit de la robe et
+          il plaça sur lui l'éphod qu'il serra avec la ceinture de l'éphod dont
+          il le revêtit. Il lui mit le pectoral, et il joignit au pectoral
+          l'urim et le thumim. Il posa la tiare sur sa tête, et il plaça sur le
+          devant de la tiare la lame d'or, diadème sacré, comme l'Éternel
+          l'avait ordonné à Moïse. Moïse prit l'huile d'onction, il oignit le
+          sanctuaire et toutes les choses qui y étaient et le sanctifia. Il en
+          fit sept fois l'aspersion sur l'autel et il oignit l'autel et tous ses
+          ustensiles, et la cuve avec sa base, afin de les sanctifier. Il
+          répandit de l'huile d'onction sur la tête d'Aaron, et l'oignit afin de
+          le sanctifier.
+        </em>
+      </p>
+      <p className="com">
+        À ce moment-là, le P.I. trace, avec son pouce droit trempé dans l'huile,
+        un « tau » sur le front du Troisième Principal assermenté.
+      </p>
+      <p>
+        <h3>NOMBRES XVI - versets 46 à 48 </h3>
+        <em>
+          Et Moïse dit à Aaron : « Prends le brasier, mets-y du feu de dessus
+          l'Autel, poses-y du parfum, va promptement vers l'assemblée et fais
+          pour eux l'expiation car la colère de l'Éternel a éclaté, la plaie a
+          commencé. » Aaron prit le brasier, comme Moïse avait dit, et courut au
+          milieu de l'assemblée ; et voici, la plaie avait commencé parmi le
+          peuple. Il offrit le parfum, et il fit l'expiation pour le peuple. Il
+          se plaça entre les morts et les vivants et la plaie fut arrêtée.
+        </em>
+      </p>
+      <p>
+        <h3> GENÈSE XXXIII - verset 20 </h3>
+        <em>Et là, il éleva un autel, qu'il appela El-Élohé Israël. </em>
+      </p>
+      <p>
+        <b>P.I. : </b>Compagnon, lors de votre Initiation, on vous a recommandé
+        d'étudier la Sainte Loi afin qu'elle soit la règle et le guide de votre
+        foi. Page 13 D'après les progrès que vous avez faits dans la
+        Franc-Maçonnerie, je ne doute pas que vous ayez constamment suivi cette
+        recommandation. Je ne vous retiendrai donc pas pour essayer d'expliquer
+        ou de commenter les passages des Saintes Écritures qui viennent d'être
+        lus. Il me suffira de faire remarquer que notre Grand Maître Moïse nous
+        a transmis, ainsi qu'à notre postérité, une preuve de la Clémence du
+        Très Haut, largement démontrée par l'envoi de Son Grand-Prêtre au milieu
+        des mourants et des morts, afin d'enrayer la peste qu'il avait
+        déclenchée pour punir Son peuple coupable. <br />
+        Prions, Compagnons.
+      </p>
+      <p className="com">Tous se tournent vers l'Est, au signe de foi. </p>
+      <p>
+        Dieu Tout Puissant, qui nous as permis de compter ce digne Compagnon
+        parmi les Chefs de l'Ordre, fais que Ta Sainte Volonté et Ton Saint Nom
+        le guident toujours dans sa vie et dans ses actes. Inspire-le et
+        inspire-nous afin que nous soyons humbles et prêts à T'obéir, à Toi et à
+        Ta Loi. Ne nous refuse pas l'indulgence dont Tu as fait preuve à l'égard
+        de Ton ancien peuple, afin qu'à l'heure dernière de notre existence,
+        nous puissions être de ceux qui, par Ta bonté, seront admis dans le
+        Temple de la Béatitude et de la Gloire éternelles.
+      </p>
+      <p>Tous : Ainsi soit-il. </p>
+      <p
+        className="com
+      "
+      >
+        (Le P.I. invite le J. assermenté à se lever.)
+      </p>
+      <p>
+        <b>P.I. : </b>Je vous confie maintenant le signe et le mot de Troisième
+        Principal. Le signe est donné comme si l'on se protégeait les narines de
+        la main gauche et si l'on balançait un encensoir de la main droite pour
+        se préserver de la peste. <br />
+        C'est une allusion au passage de l'Écriture qui vient d'être lu, dans
+        lequel Aaron fait l'expiation pour les péchés du peuple. <br />
+        Le mot est « El-Élohé Israël ». Sa signification est « Dieu, le Dieu
+        d'Israël ». Vous devez toujours répéter ce mot en silence, en même temps
+        que vous embrassez votre sceptre, avant de prendre place dans la Chaire
+        du Troisième Principal. Je vous revêts maintenant du collier portant le
+        bijou de votre charge, ainsi que de la robe correspondante.
+      </p>
+      <p className="com">
+        (Le D.C.I. pose la robe sur les épaules du J. assermenté et le P.I. en
+        croise les pans.)
+      </p>
+      <p>
+        Cette robe est aussi remarquable par la beauté de sa couleur que par la
+        solidité de son étoffe. <br />
+        Le bleu est employé fort à propos en Maçonnerie symbolique comme étant
+        la couleur caractéristique de notre Institution, laquelle a supporté
+        l'épreuve des siècles. <br />
+        Elle est l'emblème de la bienfaisance et de la charité universelle et
+        nous enseigne que ces vertus doivent être aussi grandes, dans l'esprit
+        d'un Maçon, que la voûte azurée des Cieux.
+      </p>
+      <p className="com">
+        Il lui présente le sceptre que le D.C.I. lui a tendu.
+      </p>
+      <p>
+        Je remets entre vos mains ce sceptre portant une Mitre, emblème de
+        dignité. <br />
+        Qu'il vous rappelle la dignité de l'emploi que vous occupez et que
+        l'inscription :
+      </p>
+      <p className="com">(Il la lui montre sur son sceptre.) </p>
+      <p>
+        « Sainteté au Seigneur », vous imprègne du sentiment de votre dépendance
+        vis-à-vis de Dieu. <br />
+        La perfection n'est pas de ce monde, elle n'appartient qu'au Seigneur.
+        <br />
+        Veuillez maintenant embrasser votre sceptre, répéter en vous-même le
+        mot, et prendre place dans la Chaire du Troisième Principal.
+      </p>
+      <p>
+        <b>T.E.Z. : </b> - ● - ● - ● - ● - Compagnons, je déclare que ce
+        conclave de Troisième Principaux Installés est clos.
+      </p>
+      <h2>Fin de la Cérémonie.</h2>
+    </div>
+  );
+}
